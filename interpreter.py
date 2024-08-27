f8d5bf8985d800064b1e5bc94db1b73837d644e7
simon-esp <simon.esp1814@gmail.com>
2024-08-27 17:09:15 +0200

    Rework interpreter!

--- interpreter.py
+++ interpreter.py
@@ -1,114 +1,39 @@
-import time
-import ast
-
-def get_scr(file):
-    # Fetch file and turn into 1 string, no newlines
-    try:
-        with open(file, "r") as f:
-            script = f.readlines()
-            script = ''.join([line.strip() for line in script])
-        return script
-    except Exception as e:
-        print('File was not found, or an error occurred')
-        print(f'Error: {e}')
-        return ";"
-    
-def condition(c):
-    global variables
-    try:
-        r = c.split('|')[1]
-        if r == 'F':
-            r = False
+def parse(p):
+    s =  ''.join(p.splitlines())
+    nesting = 0
+    x = []
+    y = ''
+    for i in s:
+        if i == '{':
+            nesting += 1
+        if i == '}':
+            nesting -= 1
+        if i == ';' and nesting == 0:
+            x.append(y)
+            y = ''
         else:
-            r == True
-    except:
-        r = True
-    s = c.split('|')[0]
-    operator = s.split(' ')[1]
-    match operator:
-        case '=':
-            operands = s.split(' ')
-            cond = float(eval_expression(operands[0])) == float(eval_expression(operands[2]))
-    if cond:
-        if r:return True
-        else:return False
-    else:
-        if r:return False
-        else: return True
-def eval_expression(value):
-    global variables
-    split = value.split(' ')
-    try:
-        if split[1] == '+':
-            operands = value.split(' ')
-            return float(eval_expression(operands[0])) + float(eval_expression(operands[2]))
-        if value[1] == '-':
-            operands = value.split(' ')
-            return float(eval_expression(operands[0])) - float(eval_expression(operands[2]))
-        if value[1] == '*':
-            operands = value.split(' ')
-            return float(eval_expression(operands[0])) * float(eval_expression(operands[2]))
-        if value[1] == '/':
-            operands = value.split(' ')
-            return float(eval_expression(operands[0])) / float(eval_expression(operands[2]))
-    except:
-        if value[0] == '$':
-            return variables.get(value, 0)  # Default to 0 if the variable is not found
-        if value[0] == "'" and value[-1] == "'":
-            return value[1:-1]
+            y = y+i
+    return(x)
 
-def execute_line(i):
-    global variables
-    global funcs
-    split = i.split(' ')
-    match split[0]:
+def condition(cond):
+    if cond == 'True':
+        return True
+    if cond == 'False':
+        return False
 
-        case 'v':
-            key, value = " ".join(i.split(" ")[1:]).split('=', 1)
-            value = eval_expression(value)
-            variables[key] = value
-        
-        case 'wait':
-            time.sleep(variables.get(split[1], '') if split[1][0] == '$' else " ".join(i.split(" ")[1:]))
+def interpret(s):
+    
+    for i in s:
+        keyword_gone = ' '.join(i.split(' ')[1:])
+        match i.split(' ')[0]:      
+            case 'echo':
+                print(keyword_gone)
+            case 'if':
+                if condition(keyword_gone.split(':')[0]):
+                    p = parse(':'.join(keyword_gone.split(':')[1:])[1:-1])
+                    interpret(p)
 
-        case 'echo':
-            print(variables.get(split[1], '') if split[1][0] == '$' else " ".join(i.split(" ")[1:]))
 
-        case 'rep':
-            rep_count = (variables.get(split[1], '') if split[1][0] == '$' else " ".join(i.split(" ")[1:])).split('>')[0]
-            script = ast.literal_eval('[' + ','.join(f'"{item.strip()}"' for item in (" ".join(i.split(" ")[2:]).strip().strip(';')).strip('[]').split(',')) + ']')
-            for _ in range(rep_count):
-                execute_script(script)
-
-        case 'if':
-            if condition(" ".join(i.split(" ")[1:]).strip().strip(';').split(">")[0]):
-                script = ast.literal_eval('[' + ','.join(f'"{item.strip()}"' for item in (" ".join(i.split(" ")[1:]).strip().strip(';').split(">")[1]).strip('[]').split(',')) + ']')
-                execute_script(script)
-        
-        case 'def':
-            name, script = " ".join(i.split(" ")[1:]).split('>')
-            funcs[name] = script.split(',')
-        
-        case 'call':
-            script = funcs.get(split[1])
-            print(funcs)
-            execute_script(script)
-                
-def execute_script(script):
-    global variables
-    global funcs
-    for line in script:
-        if line.strip():
-            execute_line(line.strip())
-
-def load(file):
-    global variables
-    global funcs
-    variables = {}
-    funcs = {}
-    s = get_scr(file=file)  # `s` is the script without newlines, which means it's the script to compile
-    lines = s.split(';')
-    execute_script(lines)
-
-# Example usage
-load(r"C:\Users\spoki\OneDrive\chainlink\helloworld.cks")  # Update the path as needed
+with open('/home/simonesp/test.clss', 'r') as f:
+        s = f.read()
+interpret(parse(s))
