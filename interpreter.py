7a81ab8bf72d44c5697ab3ef227baaa8cee8f03c
simon-esp <simon.esp1814@gmail.com>
2024-08-28 21:40:44 +0200

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -33,23 +33,32 @@ def condition(cond):
         return True
     if cond == 'False':
         return False
+    l = cond.split(' ')
+    if l[1] == '>':
+        return int(eval(l[0])) + int(eval(l[2]))
     
-def eval(e):
-    if str(e).startswith('$'):
-        return vars[e.strip('$')]
-    if str(e).isnumeric():
-        return e
-    if str(e).startswith('"') and e[-1] == '"':
-        return e[1:-1]
-    l = e.split(' ')
-    if l[1] == '+':
-        return eval(int(l[0])) + eval(int(l[2]))
-    if l[1] == '/':
-        return eval(int(l[0])) / eval(int(l[2]))
-    if l[1] == '*':
-        return eval(int(l[0])) * eval(int(l[2]))
-    if l[1] == '-':
-        return eval(int(l[0])) - eval(int(l[2]))
+def eval(c):
+    global vars
+    if c.startswith('e'):
+        e = c[1:]
+        if str(e).startswith('$') and not ' ' in str(e):
+            return vars[e.strip('$')]
+        if str(e).isnumeric():
+            return e
+        if str(e).startswith('"') and str(e).endswith('"'):
+            return e[1:-1]
+        l = e.split(' ')
+        if l[1] == '+':
+            return int(eval(l[0])) + int(eval(l[2]))
+        if l[1] == '++':
+            return str(eval(l[0])) + str(eval(l[2]))
+        if l[1] == '/':
+            return int(eval(l[0])) / int(eval(l[2]))
+        if l[1] == '*':
+            return int(eval(l[0])) * int(eval(l[2]))
+        if l[1] == '-':
+            return int(eval(l[0])) - int(eval(l[2]))
+    
             
 
 def interpret(s):
@@ -59,31 +68,33 @@ def interpret(s):
         keyword_gone = ' '.join(i.split(' ')[1:])
         match i.split(' ')[0]:      
             case 'echo':
-                print(vars[keyword_gone.strip('$')] if keyword_gone[0] == '$' else keyword_gone)
+                print(eval(keyword_gone))
             case 'if':
                 if condition(keyword_gone.split(':')[0]):
                     p = parse(':'.join(keyword_gone.split(':')[1:])[1:-1])
                     interpret(p)
             case 'rep':
                 p = parse(':'.join(keyword_gone.split(':')[1:])[1:-1])
-                for i in range(int(keyword_gone.split(':')[0])):
+                for i in range(int(eval(keyword_gone.split(':')[0]))):
+                    interpret(p)
+            case 'while':
+                p = parse(':'.join(keyword_gone.split(':')[1:])[1:-1])
+                while condition(keyword_gone.split(':')[0]):
                     interpret(p)
             case 'var':
-                vars[keyword_gone.split('=')[0]] = '='.join(keyword_gone.split('=')[1:])
+                vars[keyword_gone.split('=')[0]] = eval('='.join(keyword_gone.split('=')[1:]))
             case 'var+':
-                vars[keyword_gone.split('=')[0]] = int(vars[keyword_gone.split('=')[0]])+int('='.join(keyword_gone.split('=')[1:]))
+                vars[keyword_gone.split('=')[0]] = int(eval(vars[keyword_gone.split('=')[0]])+int('='.join(keyword_gone.split('=')[1:])))
             case 'call':
                 interpret(funcs[keyword_gone])
             case 'input':
-                vars[keyword_gone.split(':')[0]] = input(':'.join(keyword_gone.split(':')[1:]))
+                vars[keyword_gone.split(':')[0]] = input(eval(':'.join(keyword_gone.split(':')[1:])))
 
 global vars
 vars = {}
 funcs = {}
-with open('/home/simonesp/test.clss', 'r') as f:
+with open('C:\\Users\\spoki\\OneDrive\\chainlink\\test.clss', 'r') as f:
         s = f.read()
 script = parse(s)
 find_functions(script)
 interpret(script)
-print(eval('10 * 10'))
-print(eval('"hello world"'))
